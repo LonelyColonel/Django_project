fbed2c49f827007a85235009da0fa36818f7c385
Lonely_Colonel_PC <lonely_colonel@mail.ru>
2022-10-17 18:46:58 +0300

    Task1 corrections

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -7,9 +7,9 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = os.getenv('DEBUG')
+DEBUG = os.environ.get('DEBUG') == 'True'
 
 ALLOWED_HOSTS = []
 
